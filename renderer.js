1b0570b035b1eac1fe4a6c775e877c2cc1de9b7e
slavik <slavikshen@gmail.com>
2017-11-15 09:40:08 -0500

    undefine variable cause v8 IPC error

--- renderer.js
+++ renderer.js
@@ -4,47 +4,52 @@ module.exports = null;
 
 var ipcRenderer;
 try {
-  ipcRenderer = require('electron').ipcRenderer;
+	ipcRenderer = require('electron').ipcRenderer;
 } catch (e) {
-  ipcRenderer = null;
+	ipcRenderer = null;
 }
 
 var originalConsole = require('./lib/original-console');
 
 if (ipcRenderer) {
-  module.exports = {
-    error:   log.bind(null, 'error'),
-    warn:    log.bind(null, 'warn'),
-    info:    log.bind(null, 'info'),
-    verbose: log.bind(null, 'verbose'),
-    debug:   log.bind(null, 'debug'),
-    silly:   log.bind(null, 'silly'),
-    log:     log.bind(null, 'info')
-  };
+	module.exports = {
+		error:	 log.bind(null, 'error'),
+		warn:		log.bind(null, 'warn'),
+		info:		log.bind(null, 'info'),
+		verbose: log.bind(null, 'verbose'),
+		debug:	 log.bind(null, 'debug'),
+		silly:	 log.bind(null, 'silly'),
+		log:		 log.bind(null, 'info')
+	};
 
-  module.exports.default = module.exports;
+	module.exports.default = module.exports;
 
-  ipcRenderer.on('__ELECTRON_LOG_RENDERER__', function(event, level, text) {
-    if (level === 'verbose') {
-      level = 'log';
-    } else if (level === 'silly') {
-      level = 'debug';
-    }
+	ipcRenderer.on('__ELECTRON_LOG_RENDERER__', function(event, level, text) {
+		if (level === 'verbose') {
+			level = 'log';
+		} else if (level === 'silly') {
+			level = 'debug';
+		}
 
-    originalConsole[level](text);
-  });
+		originalConsole[level](text);
+	});
 }
 
 function log() {
-  var data = Array.prototype.slice.call(arguments);
+	var data = Array.prototype.slice.call(arguments);
 
-  data = data.map(function(obj) {
-    if (obj instanceof Error) {
-      obj = obj.stack || obj;
-    }
+	data = data.map(function(obj) {
 
-    return obj;
-  });
+		if( obj === undefined ) {
+			obj = 'undefined';
+		} else if (obj instanceof Error) {
+			obj = obj.stack || obj;
+		} else if( obj instanceof Object ) {
+			obj = JSON.stringify(obj);
+		}
 
-  ipcRenderer.send('__ELECTRON_LOG__', data);
+		return obj;
+	});
+
+	ipcRenderer.send('__ELECTRON_LOG__', data);
 }
